7d060ff9182d3f594328fb882b69556b79ed6af9
Zeynep Sude Sümer <235060003@toros.edu.tr>
2026-05-10 23:10:15 +0200

    Update app.py
    Created app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Smart Study Planner"
+study_tasks = []
 
-if __name__ == "__main__":
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        task = request.form.get('task')
+        study_tasks.append(task)
+
+    return render_template('index.html', tasks=study_tasks)
+
+if __name__ == '__main__':
     app.run(debug=True)
